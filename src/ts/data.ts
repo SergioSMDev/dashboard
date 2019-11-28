c69948b57ec8164d53d481bfddd49ec0cb7d5898
Sergey <56604489+SergioSMDev@users.noreply.github.com>
2019-11-28 22:48:48 +0200

    Update data.ts

--- src/ts/data.ts
+++ src/ts/data.ts
@@ -1,5 +1,8 @@
+import cards from "../../cards.json";
+
 export async function fetchAsync() {
-  let response = await fetch("cards.json");
-  let data = await response.json();
-  return data;
+//   let response = await fetch("cards.json");
+//   let data = await response.json();
+//   return data;
+  return cards;
 }
